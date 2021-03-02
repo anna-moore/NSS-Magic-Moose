d98ecae03491c669de7f09cda0596cc682938736
anna-moore <ammoore434@gmail.com>
2021-03-02 16:21:43 -0600

    errors

--- Program.cs
+++ Program.cs
@@ -1,29 +1,37 @@
 ﻿using System;
-
+//still gives one more answer after white space is entered
+bool stillAsking = true;
 Main();
 
 void Main()
 {
     Console.WriteLine("Welcome to the Magic Moose!!");
     Console.WriteLine("--------------------------------------------");
-
-
-
-    //let the moose ask a question
-    MooseAnswers("this is an answer");
+    while (stillAsking)
+    {
+        MooseAsk();
+    }
 }
 
 void MooseAsk()
 {
-    Console.WriteLine("Ask me Anything");
-    Console.ReadLine();
+    Console.Write("Ask me Anything: ");
+    string question = Console.ReadLine();
+    if (String.IsNullOrWhiteSpace(question))
+    {
+        stillAsking = false;
+        MooseAnswers("Bye!");
+        return;
+    }
+    int caseSwitch = RandomNumber();
+    MooseAnswers(MagicMooseGetAnswer(caseSwitch));
 }
 
 int RandomNumber()
 {
     Random r = new Random();
     int genRand = r.Next(0, 19);
-    Console.WriteLine("Random Number = " + genRand);
+    // Console.WriteLine("Random Number = " + genRand);
     return genRand;
 }
 void MooseAnswers(string answer)
@@ -57,67 +65,71 @@ void MooseAnswers(string answer)
                        `^^` `^^^`
     ");
 }
-
-
+string MagicMooseGetAnswer(int caseSwitch)
 {
-    case 0:
-        Console.WriteLine("As I see it, yes.");
-    break;
-    case 1:
-        Console.WriteLine("Ask again later.");
-    break;
-    case 2:
-        Console.WriteLine("Better not tell you now.");
-    break;
-    case 3:
-        Console.WriteLine("Cannot predict now.");
-    break;
-    case 4:
-        Console.WriteLine(" Concentrate and ask again.");
-    break;
-    case 5:
-        Console.WriteLine("Don’t count on it.");
-    break;
-    case 6:
-        Console.WriteLine(" It is certain.");
-    break;
-    case 7:
-        Console.WriteLine("It is decidedly so.");
-    break;
-    case 8:
-        Console.WriteLine("Most likely.");
-    break;
-    case 9:
-        Console.WriteLine("My reply is no.");
-    break;
-    case 10:
-        Console.WriteLine("My sources say no.");
-    break;
-    case 11:
-        Console.WriteLine("Outlook not so good.");
-    break;
-    case 12:
-        Console.WriteLine("Outlook good.");
-    break;
-    case 13:
-        Console.WriteLine("Reply hazy, try again.");
-    break;
-    case 14:
-        Console.WriteLine("Signs point to yes.");
-    break;
-    case 15:
-        Console.WriteLine("Very doubtful.");
-    break;
-    case 16:
-        Console.WriteLine("Without a doubt.");
-    break;
-    case 17:
-        Console.WriteLine("Yes.");
-    break;
-    case 18:
-        Console.WriteLine("Yes – definitely.");
-    break;
-    case 19:
-        Console.WriteLine("You may rely on it.");
-    break;
+    string answer = " ";
+    switch (caseSwitch)
+    {
+        case 0:
+            answer = "As I see it, yes.";
+            return answer;
+        case 1:
+            answer = "Ask again later.";
+            return answer;
+        case 2:
+            answer = "Better not tell you now.";
+            return answer;
+        case 3:
+            answer = "Cannot predict now.";
+            return answer;
+        case 4:
+            answer = " Concentrate and ask again.";
+            return answer;
+        case 5:
+            answer = "Don’t count on it.";
+            return answer;
+        case 6:
+            answer = " It is certain.";
+            return answer;
+        case 7:
+            answer = "It is decidedly so.";
+            return answer;
+        case 8:
+            answer = "Most likely.";
+            return answer;
+        case 9:
+            answer = "My reply is no.";
+            return answer;
+        case 10:
+            answer = "My sources say no.";
+            return answer;
+        case 11:
+            answer = "Outlook not so good.";
+            return answer;
+        case 12:
+            answer = "Outlook good.";
+            return answer;
+        case 13:
+            answer = "Reply hazy, try again.";
+            return answer;
+        case 14:
+            answer = "Signs point to yes.";
+            return answer;
+        case 15:
+            answer = "Very doubtful.";
+            return answer;
+        case 16:
+            answer = "Without a doubt.";
+            return answer;
+        case 17:
+            answer = "Yes.";
+            return answer;
+        case 18:
+            answer = "Yes – definitely.";
+            return answer;
+        case 19:
+            answer = "You may rely on it.";
+            return answer;
+    }
+    return answer;
 }
